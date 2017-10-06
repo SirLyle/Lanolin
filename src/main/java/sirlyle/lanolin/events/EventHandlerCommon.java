ed43957a193daa56679907f9985e9cd3b41978d0
SirLyle <infraredhat@gmail.com>
2017-10-05 20:19:32 -0400

    Issue #2. Pesky @Side decorations.

--- src/main/java/sirlyle/lanolin/events/EventHandlerCommon.java
+++ src/main/java/sirlyle/lanolin/events/EventHandlerCommon.java
@@ -11,9 +11,12 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EventHandlerCommon {
     @SubscribeEvent
+    @SideOnly(Side.CLIENT)
     public void onLanolinTooltip(ItemTooltipEvent event){
         ItemStack stack = event.getItemStack();
         if (!stack.isEmpty() && stack.getItem() instanceof ItemArmor && stack.hasTagCompound()
