7e28f8de32c3be165d412e3f167649fec4bca205
SirLyle <infraredhat@gmail.com>
2017-10-05 20:46:03 -0400

    Reorganize some assets which aren't part of the final .jar into their own Art directory. Added a tooltip to ItemLanolin

---
+++ src/main/java/sirlyle/lanolin/compat/jei.java
@@ -0,0 +1,10 @@
+package sirlyle.lanolin.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JEIPlugin;
+
+@JEIPlugin
+public class jei implements IModPlugin {
+    // Add a description to JEI when a user checks the uses for lanolin.
+
+}
--- src/main/java/sirlyle/lanolin/items/ItemLanolin.java
+++ src/main/java/sirlyle/lanolin/items/ItemLanolin.java
@@ -1,12 +1,20 @@
 package sirlyle.lanolin.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import sirlyle.lanolin.Lanolin;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemLanolin extends Item {
     public ItemLanolin() {
@@ -18,5 +26,15 @@ public class ItemLanolin extends Item {
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
+        tooltip.add(getToolTipData());
+    }
+
+    private String getToolTipData(){
+        return  I18n.format("item.lanolin.lanolin.tooltip");
     }
 }
